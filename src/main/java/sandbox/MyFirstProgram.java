df132d9dca8df712bb598ad5abdb539360d450c7
NickSamara <nikoqa10@mail.com>
2019-10-06 19:21:52 +0200

    Добавлены арифметические примеры

---
+++ src/main/java/sandbox/MyFirstProgram.java
@@ -0,0 +1,36 @@
+package sandbox;
+
+import java.sql.SQLOutput;
+
+public class MyFirstProgram {
+
+ public static void main(String[] args) {
+
+ 	System.out.println("Hello, world !");
+
+	 System.out.println(2 + 2);
+	 System.out.println(2 * 2);
+	 System.out.println(2 / 2);
+	 System.out.println(2 - 2);
+
+	 System.out.println(1 / 2);
+	 System.out.println(1.0 / 2);
+	 System.out.println(1 / 2.0);
+	 System.out.println(2.0 / 2);
+	 System.out.println("2" + "2");
+	 System.out.println("2" + 2);
+	 System.out.println(2 + "2");
+
+	 System.out.println(2 + 2 * 2);
+	 System.out.println((2 + 2) * 2);
+	 System.out.println("2 + 2 = " + 2 + 2);
+	 System.out.println("2 + 2 = " + (2 + 2));
+
+		int l = 8;
+		int s = l + l;
+	 System.out.println("Площадь квадрата со стороной " + l + " = " + (l * l));
+	}
+
+
+
+}
